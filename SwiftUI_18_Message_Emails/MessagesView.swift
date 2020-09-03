b2fed35f3a20766aea9df6f1d699223b8ec57282
Alan Casas Arevalo <alan.casas.arevalo@everis.com>
2020-09-03 14:43:21 +0200

    [FEATURE][MESSAGE] * Send message was implemented

--- SwiftUI_18_Message_Emails/MessagesView.swift
+++ SwiftUI_18_Message_Emails/MessagesView.swift
@@ -7,17 +7,19 @@ struct MessagesView: View {
     @State private var phone = ""
     @State private var message = ""
     
+    private let messageComposeViewControllerDelegate = MessageDelegate()
+    
     var body: some View {
         NavigationView {
             VStack {
                 TextField("Telefono", text: self.$phone)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.namePhonePad)
+                    .keyboardType(.phonePad)
                 TextField("Message", text: self.$message)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 
                 Button(action: {
-                    
+                    self.sendMessage()
                 }) {
                     Text("Enviar el mensaje")
                         .font(.system(.largeTitle, design: .rounded))
@@ -32,7 +34,25 @@ struct MessagesView: View {
 }
 
 extension MessagesView {
+    class MessageDelegate: NSObject, MFMessageComposeViewControllerDelegate {
+        func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+            controller.dismiss(animated: true)
+        }
+    }
     
+    func sendMessage() {
+        if MFMessageComposeViewController.canSendText() {
+            let viewController = UIApplication.shared.windows.filter { $0.isKeyWindow }.first?.rootViewController
+            let sms = MFMessageComposeViewController()
+            sms.body = self.message
+            sms.recipients = [self.phone]
+            
+            sms.messageComposeDelegate = messageComposeViewControllerDelegate
+            viewController?.present(sms, animated: true)
+        } else {
+            print("No se puede enviar el mensaje")
+        }
+    }
 }
 
 struct MessagesView_Previews: PreviewProvider {
